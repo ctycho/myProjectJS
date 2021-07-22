9b2f0c8ef3a362f81f7b408fb3b5231aa81048dc
ctycho <ilnur_gumerov_1996@mail.ru>
2021-07-22 18:06:22 +0300

    practice 1

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 'use strict';
 
-const numberOfFilms = prompt("How many movies have u watched?", "");
+const numberOfFilms = +prompt('How many movies have u watched?', '');
+if (numberOfFilms < 10) {
+    alert('That\'s not a lot');
+} else if (numberOfFilms < 30) {
+    alert('You\'re a good viewer');
+} else if (numberOfFilms >= 30)
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,12 +15,15 @@ const personalMovieDB = {
     privat: false,
 };
 
-const a = prompt('What is your favourite movie?', ''),
-    b = prompt('How would you rate it?', ''),
-    c = prompt('What is your favourite movie?', ''),
-    d = prompt('How would you rate it?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; ++i) {
+    const a = prompt('What is your favourite movie?', ''),
+        b = prompt('How would you rate it?', '');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    }
+    else {
+        i--;
+    }
+}
 
 console.log(personalMovieDB);
